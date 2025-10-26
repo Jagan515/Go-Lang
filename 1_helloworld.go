e26eae76e389ef1d5827dcf57f3c4d48299fd27b
Jagan Pradhan <jaganp515@gmail.com>
2025-10-27 02:35:09 +0530

    First commit hello-worls

---
+++ 1_helloworld.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("HelloWorld")
+}
+
+// go build 1_helloworld.go produce executable file
+// ./1_helloworld
+
+// to run it directlty without building
+// go run 1_helloworld.go
